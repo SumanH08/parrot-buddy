0ed80ca73a91d521513fc55ea3b6592eda0850e7
Suman Hiremath <suman.hiremath89@gmail.com>
2018-01-20 23:06:37 +0530

    Revert "added t"
    This reverts commit c44cdb3660e806ae364771b032c703cbb1e5e962.

--- src/components/Treatment.vue
+++ src/components/Treatment.vue
@@ -12,47 +12,17 @@
       <tr>
         <td><input type="checkbox" checked data-toggle="toggle" data-on=""></td>
         <td>Maxalt</td>
-        <td>
-          <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Rescue Med
-    <span class="caret"></span></button>
-            <ul class="dropdown-menu">
-              <li><a href="#">Relief</a></li>
-              <li><a href="#">Preventive</a></li>
-              <li><a href="#">Others</a></li>
-            </ul>
-          </div>
-        </td>
+        <td>Dropdown</td>
       </tr>
       <tr>
         <td><input type="checkbox" checked data-toggle="toggle" data-on=""></td>
         <td>Caffeine</td>
-        <td>
-          <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Rescue Med
-    <span class="caret"></span></button>
-            <ul class="dropdown-menu">
-              <li><a href="#">Relief</a></li>
-              <li><a href="#">Preventive</a></li>
-              <li><a href="#">Others</a></li>
-            </ul>
-          </div>
-        </td>
+        <td>Dropdown</td>
       </tr>
       <tr>
         <td><input type="checkbox" checked data-toggle="toggle" data-on=""></td>
         <td>Cold Shower</td>
-        <td>
-          <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Rescue Med
-    <span class="caret"></span></button>
-            <ul class="dropdown-menu">
-              <li><a href="#">Relief</a></li>
-              <li><a href="#">Preventive</a></li>
-              <li><a href="#">Others</a></li>
-            </ul>
-          </div>
-        </td>
+        <td>Dropdown</td>
       </tr>
     </tbody>
   </table>
