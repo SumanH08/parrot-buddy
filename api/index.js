d86860fa3cebdef81848128a4e6fa26426e51f46
Suman Hiremath <suman.hiremath89@gmail.com>
2018-01-23 20:41:19 +0530

    Mock server

---
+++ api/index.js
@@ -0,0 +1,55 @@
+var bodyParser = require('body-parser')
+const fs = require('fs')
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/api/recent', function(req, res) {
+    res.set("Content-Type", "application/json");
+    var recent = fs.readFileSync(__dirname+"/recent.json");
+    res.send(recent);
+})
+
+app.get('/api/settings', function(req, res) {
+    res.set("Content-Type", "application/json");
+    var settings = fs.readFileSync(__dirname+"/settings.json");
+    res.send(settings);
+})
+
+app.put('/api/recent', function(req, res) {
+    var recent = fs.readFileSync(__dirname+"/recent.json", 'utf-8');
+    recent = JSON.parse(recent);
+    var userObj = req.body;
+
+    recent = recent.map(function(obj) {
+      if(obj.date == userObj.date) {
+        return userObj;
+      }
+
+      return obj;
+    })
+
+    fs.writeFileSync(__dirname+"/recent.json", JSON.stringify(recent));
+    res.send('{"status": "ok"}')
+})
+
+app.put('/api/settings', function(req, res) {
+    var settings = fs.readFileSync(__dirname+"/settings.json", 'utf-8');
+    settings = JSON.parse(settings);
+    var userObj = req.body;
+
+    settings = settings.map(function(obj) {
+      if(obj.Name == userObj.Name) {
+        return userObj;
+      }
+
+      return obj;
+    })
+
+    fs.writeFileSync(__dirname+"/settings.json", JSON.stringify(settings));
+    res.send('{"status": "ok"}')
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
